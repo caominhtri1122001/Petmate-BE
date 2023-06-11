0472b99fff2fc72503752c6dc3e1c3bdf9c75457
Nguyễn Khoa Hoàng <blcm2486@gmail.com>
2023-06-11 11:30:54 +0700

    feat: crud blog admin

--- src/main/java/com/example/petmate/service/post/PostServiceImpl.java
+++ src/main/java/com/example/petmate/service/post/PostServiceImpl.java
@@ -88,6 +88,26 @@ public class PostServiceImpl implements PostService {
 			post.get().setImage(image);
 		}
 		postRepository.save(post.get());
+
+		List<Category> categories = categoryRepository.findTagsOfPost(UUID.fromString(postId));
+		if (!categories.isEmpty()){
+			categories.forEach(category -> {
+				categoryRepository.deleteById(category.getId());
+			});
+		}
+
+		List<Tag> tags = tagRepository.findAll();
+		request.getTags().forEach(tag -> {
+			Tag tagExists = tags.stream().filter(t -> t.getName().equalsIgnoreCase(tag)).findFirst().orElse(null);
+			if (tagExists != null) {
+				createCategory(post.get().getId(), tagExists.getId());
+			} else {
+				Tag tagEntity = Tag.builder().name(tag).build();
+				tagRepository.save(tagEntity);
+				createCategory(post.get().getId(), tagEntity.getId());
+			}
+		});
+
 		return true;
 	}
 
@@ -106,6 +126,14 @@ public class PostServiceImpl implements PostService {
 		if (post.isEmpty()) {
 			throw new ResponseException(ResponseCodes.PM_NOT_FOUND);
 		}
+
+		List<Category> categories = categoryRepository.findTagsOfPost(UUID.fromString(postId));
+		if (!categories.isEmpty()){
+			categories.forEach(category -> {
+				categoryRepository.deleteById(category.getId());
+			});
+		}
+
 		postRepository.deleteById(UUID.fromString(postId));
 		return true;
 	}
