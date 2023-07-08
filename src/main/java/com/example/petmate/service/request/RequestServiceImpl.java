834ba91e61618d32ad43068e225724c1d3ac5c5a
caominhtri112201 <caominhtri112201@gmail.com>
2023-07-08 18:49:33 +0700

    hotfix/PM: Validate create request

--- src/main/java/com/example/petmate/service/request/RequestServiceImpl.java
+++ src/main/java/com/example/petmate/service/request/RequestServiceImpl.java
@@ -59,6 +59,10 @@ public class RequestServiceImpl implements RequestService {
 		List<Request> requestSchedules = requests.stream()
 				.filter(rq -> StatusType.PENDING.getType().equals(rq.getStatus()) && rq.isValid())
 				.collect(Collectors.toList());
+		List<Request> acceptedRequests = requests.stream()
+				.filter(rq -> StatusType.ACCEPT.getType().equals(rq.getStatus()) && rq.isValid())
+				.collect(Collectors.toList());
+		requestSchedules.addAll(acceptedRequests);
 		if (validateRequest(newRequest, requestSchedules)) {
 			throw new ResponseException(ResponseCodes.PM_NOT_AVAILABLE);
 		}
@@ -242,9 +246,17 @@ public class RequestServiceImpl implements RequestService {
 					existingRequest.getStartDate() + " " + existingRequest.getStartTime(), formatter);
 			LocalDateTime existingEndDateTime = LocalDateTime.parse(
 					existingRequest.getEndDate() + " " + existingRequest.getEndTime(), formatter);
-			if ((startDateTime.isAfter(existingStartDateTime) && startDateTime.isBefore(existingEndDateTime)) || (
-					endDateTime.isAfter(existingStartDateTime) && endDateTime.isBefore(existingEndDateTime)) || (
-					startDateTime.isEqual(existingStartDateTime) && endDateTime.isEqual(existingEndDateTime))) {
+//			if ((startDateTime.isAfter(existingStartDateTime) && startDateTime.isBefore(existingEndDateTime)) || (
+//					endDateTime.isAfter(existingStartDateTime) && endDateTime.isBefore(existingEndDateTime)) || (
+//					startDateTime.isEqual(existingStartDateTime) && endDateTime.isEqual(existingEndDateTime))) {
+//				return true;
+//			}
+			if ((startDateTime.isAfter(existingStartDateTime) && startDateTime.isBefore(existingEndDateTime)) ||
+					(endDateTime.isAfter(existingStartDateTime) && endDateTime.isBefore(existingEndDateTime)) ||
+					(startDateTime.isEqual(existingStartDateTime) && endDateTime.isEqual(existingEndDateTime)) ||
+					(existingStartDateTime.isAfter(startDateTime) && existingStartDateTime.isBefore(endDateTime)) ||
+					(existingEndDateTime.isAfter(startDateTime) && existingEndDateTime.isBefore(endDateTime)) ||
+					(existingStartDateTime.isEqual(startDateTime) && existingEndDateTime.isEqual(endDateTime))) {
 				return true;
 			}
 		}
